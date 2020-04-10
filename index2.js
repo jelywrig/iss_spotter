fe605b2591ea9839e7ac44c18b000da3b5a5447d
Jessie Wright <jessie.wright@gmail.com>
2020-04-10 00:12:56 +0000

    cleaned up with a printPassTime function

--- index2.js
+++ index2.js
@@ -1,12 +1,17 @@
 const {nextISSTimesForMyLocation} = require('./iss_promised');
 
-nextISSTimesForMyLocation()
-.then(response => {
+const printPassTimes = (response) => {
   for(let pass of response) {
     const date = new Date(0);
     date.setUTCSeconds(pass.risetime);
     console.log(`Next pass at ${date} for ${pass.duration} seconds!`);
   }  
+};
+
+
+nextISSTimesForMyLocation()
+.then(response => {
+  printPassTimes(response);
 })
 .catch(error => {
   console.log("It didn't work", error.message);
