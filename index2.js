3aac0070a0c870dcf76c025a8e6d0df2ccf017e4
Jessie Wright <jessie.wright@gmail.com>
2020-04-10 00:05:35 +0000

    implemented error handling

--- index2.js
+++ index2.js
@@ -7,4 +7,7 @@ nextISSTimesForMyLocation()
     date.setUTCSeconds(pass.risetime);
     console.log(`Next pass at ${date} for ${pass.duration} seconds!`);
   }  
+})
+.catch(error => {
+  console.log("It didn't work", error.message);
 });
